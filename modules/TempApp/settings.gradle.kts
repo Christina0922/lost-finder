a5cc5c2f991b8a4695932b11ef10554d40a7d855
Yoonjeong Cho <yoonjeongc@gmail.com>
2025-07-12 23:55:14 +0900

    메인페이지 UI 개선 및 모바일 여백 최적화

---
+++ modules/TempApp/settings.gradle.kts
@@ -0,0 +1,20 @@
+// 📄 settings.gradle.kts
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LostFinderProject"
+include(":modules:TempApp")
